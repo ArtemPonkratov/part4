9f7ede3e5693fae6bb8df1a7c02e1bc66ea93707
ArtemPonkratov <artemka.g@mail.ru>
2023-04-15 13:41:20 +0200

    partr4

---
+++ part4/main.js
@@ -0,0 +1,26 @@
+let a = 10
+let b = 5
+
+if ( a + b > 15) {
+    console.log("Yes");
+}
+
+else if (a+b == 15){
+    console.log("Равно 15");
+}
+else {
+    console.log("No");
+}
+
+let a = "5";
+let b = 5;
+if (a === b) {
+  console.log("Переменные равны");
+} else {
+  console.log("Переменные не равны");
+}
+
+let result = (a === b);
+console.log(result);
+
+
